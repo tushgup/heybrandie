4b77296df7046dc31e35519fab629e6f4892ccd3
Tushar Gupta <tush1998@gmail.com>
2017-12-20 14:03:21 +0530

    Conf.js added!

---
+++ messenger-bot/conf.js
@@ -0,0 +1,6 @@
+var conf = module.exports = {
+    FB_MESSAGE_URL: 'https://graph.facebook.com/v2.6/me/messages',
+    VERIFY_TOKEN: '<VERIFICATION_TOKEN>',
+    PROFILE_TOKEN: '<PAGE_ACCESS_TOKEN>',
+    PORT: '3000'
+};
